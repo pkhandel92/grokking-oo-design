79f52fac6ae47138fdabea5f82890dbbcb2cf88f
abhijay-kumar <abhijay.kumar@wissen.com>
2020-10-01 18:02:10 +0530

    Review comment - stack-overflow

--- stack-overflow/src/main/java/com/overlow/stack/account/Account.java
+++ stack-overflow/src/main/java/com/overlow/stack/account/Account.java
@@ -1,6 +1,14 @@
 package com.overlow.stack.account;
 
+
+
 public abstract class Account {
+  
+  /*
+   * #resetPassword(String newPassword) is a service method. It should not exist in the class with data fields.
+   * It's breaking single responsibility principal.
+   */
+  
     private String name;
     private String address;
     private String email;
